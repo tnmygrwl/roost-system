1abe8ca953f23167176b334d93bd763a1754ac4a
Dan Sheldon <sheldon@swarm2.cm.cluster>
2021-06-07 14:48:54 -0400

    Specify that wsrlib and detectron2 can be installed from github

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 install_requires = [
     "torch",
+    "opencv-python",
     ### data ###
     "pytz",             # time zone, 2021.1
     "ephem",            # 3.7.7.1
     "geopy",            # 2.1.0
     ### system ###
-    'wsrlib',
-    'detectron2',
+    'wsrlib @ git+https://github.com/darkecology/pywsrlib#egg=wsrlib',
+    'detectron2 @ git+https://github.com/facebookresearch/detectron2.git#egg=detectron2',
     'imageio',          # saving gif, 2.9.0
     'scikit-learn',     # nearest neighbor search using ball tree for wind farm, 0.24.2
 ]
