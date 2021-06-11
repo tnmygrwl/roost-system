d3f0b6659105e16c4f8fda901e875ae14a1c67db
Dan Sheldon <sheldon@swarm2.cm.cluster>
2021-06-10 21:42:08 -0400

    add comment

--- tools/publish_images.sh
+++ tools/publish_images.sh
@@ -6,6 +6,8 @@ HOST=${3:-doppler.cs.umass.edu}
 DST=${4:-/var/www/html/roost/img}
 
 ssh $HOST mkdir -p $DST/$NAME
+
+# change after source folder structure is updated
 rsync -avz $SRC/$NAME/roosts_ui_data/ref0.5_images/ $HOST:$DST/$NAME/dz05
 rsync -avz $SRC/$NAME/roosts_ui_data/rv0.5_images/ $HOST:$DST/$NAME/vr05
 
